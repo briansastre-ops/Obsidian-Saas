a961f43f17ded3165234c03cd11420bcd67946fe
briansastre-ops <briansastre158@gmail.com>
2024-10-17 09:45:12 -0300

    Added Compmnents and Hero

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -49,6 +49,10 @@ const Header = () => {
                         </ul>
                     </nav>
                 </div>
+                <div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+                    <img src="/images/bg-outlines.svg"  width={960} height={380} alt="outline" className='relative z-2'/>
+                    <img src="/images/bg-outlines-fill.png"  width={960} height={380} alt="outline" className='absolute inset-0 mix-blend-soft-light opacity-5'/>
+                </div>
             </div>
             <button className='lg:hidden z-2 size-10 border-2 border-s-4/25 rounded-full flex justify-center items-center' onClick={()=>setIsOpen((prevState)=>!prevState)}>
                 <img
